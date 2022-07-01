4f0c3e5d659270e5160905c1a582b347d3968159
Nyam07 <bkamunya77@gmail.com>
2022-07-01 17:53:46 +0300

    Create serializer and add functionalities for the Api

--- I4G028026TZN/links/views.py
+++ I4G028026TZN/links/views.py
@@ -1,3 +1,26 @@
+from tkinter.tix import Tree
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from I4G028026TZN.links.serializers import LinkSerializer
 
-# Create your views here.
+from .models import Link
+
+class PostListApi(ListAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostCreateApi(CreateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostDetailApi(RetrieveAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+class PostUpdateApi(UpdateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer()
+
+class PostDeleteApi(DestroyAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer()
